d591d2fa46e1c1b49a8affc5312f2537536ebf1b
anly22 <alysokhmara@gmail.com>
2024-03-16 16:21:00 +0200

    endpoint /health is added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
+
+@app.route('/health')
+def gethealth():
+    print('Health')
+    return Response(status=200)
 
 
 if __name__ == '__main__':

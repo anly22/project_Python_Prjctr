0f4dddc4813a8e3102a3414f095d32078109eafc
anly22 <alysokhmara@gmail.com>
2024-03-19 17:25:32 +0200

    Test without storage and actions

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from flask import Flask, Response, jsonify, request
 import json
-# import numpy as np
-import storage as s
-import actions as a
+import numpy as np
+# import storage as s
+# import actions as a
 
 app = Flask(__name__)
 
+game_database = {
+    'balance': None,
+    'map_size': None,
+    'team': None,
+    'round': 0
+    }
 
+agents = []
+
+map_array = []
+
+
+def get_map(size: int):
+    map_array = np.zeros((size, size))
+    return map_array
 @app.route('/health')
 def gethealth():
     return Response(status=200)
@@ -16,12 +30,11 @@ def gethealth():
 def toInit():
     if request.is_json:
         r = request.get_json()
-        s.game_database['map_size'] = r['map_size']
-        s.game_database['balance'] = r['init_balance']
-        s.game_database['team'] = r['team']
-        a = r['map_size']
-        s.map_array = s.get_map(a)
-        print("INIT", s.game_database)
+        game_database['map_size'] = r['map_size']
+        game_database['balance'] = r['init_balance']
+        game_database['team'] = r['team']
+        map_array = get_map(r['map_size'])
+        print("INIT", game_database)
         return Response(status=200)
     else:
         return Response(status=404)
@@ -32,9 +45,9 @@ def toInit():
 def toRound():
     if request.is_json:
         r = request.get_json()
-        s.game_database['balance'] = r['balance']
-        s.game_database['round'] = r['round']
-        print('ROUND', s.game_database)
+        game_database['balance'] = r['balance']
+        game_database['round'] = r['round']
+        print('ROUND', game_database)
         return Response(status=200)
     else:
         return Response(status=404)
@@ -45,8 +58,9 @@ def toRound():
 def toInitAgent(id):
     if request.is_json:
         agent_info = request.get_json()
-        s.agents.append(agent_info)
-        print(s.agents)
+        agents.append(agent_info)
+        for a in agents: 
+            print('AGENT', a)
         # return  jsonify(s.agents), 200
         return Response(status=200)
     else:
@@ -54,15 +68,12 @@ def toInitAgent(id):
 
 
 @app.route('/agent/<int:id>/action', methods=['GET'])
-def toCallAgent_Act(id):
-    res = jsonify(a.define_action(id))
-    res.headers['Content-Type'] = 'application/json'
+def toCallAct(id):
+    print(id, 'BUILD BOT AGENT')
+    build = jsonify({"type": "BUILD_BOT", "params": {"d_loc": [-1, 1]}})
 
-    for a in s.agents: 
-        print(a, 'AGENTS')
-        
-    return res, 200
-
+    return build, 200
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
